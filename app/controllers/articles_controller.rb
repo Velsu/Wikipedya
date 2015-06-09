9124845057b2083c32b72e61f3f283f0a83dde91
Velsu <riftmage50@gmail.com>
2015-06-09 18:48:50 +0200

    Add authentication, configure controller to associate new articles with current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
 	before_action :find_article, only: [:show]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@articles = Article.all.order(created_at: :desc)
@@ -10,11 +11,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-		@article = Article.new
+		@article = current_user.articles.build
 	end
 
 	def create
-		@article = Article.create(article_params)
+		@article = current_user.articles.build(article_params)
 
 		if @article.save
 			redirect_to @article
