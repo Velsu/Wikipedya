5aa9209771e8ae342eb44b543687a309f23e45b4
Velsu <riftmage50@gmail.com>
2015-06-09 20:11:51 +0200

    Implement filters for viewing articles based on categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,12 @@ class ArticlesController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		@articles = Article.all.order(created_at: :desc)
+		if params[:category].blank?
+			@articles = Article.all.order(created_at: :desc)
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@articles = Article.where("category_id = ?", @category_id)
+		end
 	end
 
 	def show
